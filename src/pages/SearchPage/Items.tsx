48172db0d05e4a7e7ddf61675139c107525d62bd
irieryota <ryotairie123@gmail.com>
2019-09-30 02:43:23 +0900

    feat: とりあえずpaginationの動作確認完了

--- src/pages/SearchPage/Items.tsx
+++ src/pages/SearchPage/Items.tsx
@@ -1,18 +1,21 @@
-import React, { useCallback, useContext } from "react";
-import { SearchItemPageContext } from "./Page";
+import React, { useCallback, useContext, useMemo } from "react";
+import { SearchItemPageContext, SearchItemPageProps } from "./Page";
+import { searchItemsImpl } from "../../reqs/searchItems";
 
-export const Items: React.FC = () => {
+type Props = Pick<SearchItemPageProps, "searchItems">
+
+export const Items: React.FC<Props> = ({ searchItems = searchItemsImpl }) => {
 
     const ctx = useContext(SearchItemPageContext)
 
-    const renderer = useCallback(() => {
+    const itemsRenderer = useCallback(() => {
         switch (true) {
             case ctx.isItemLoading:
                 return <div>loading...</div>
-            case ctx.items.length === 0:
+            case ctx.itemsResponse.items.length === 0:
                 return <div>no items</div>
             default:
-                const items = ctx.items.map((item, i, all) =>
+                const items = ctx.itemsResponse.items.map((item, i, all) =>
                     <div key={item.name}>
                         {item.name}
                     </div>
@@ -21,7 +24,49 @@ export const Items: React.FC = () => {
                     {items}
                 </div>
         }
-    }, [ctx.isItemLoading, ctx.items])
+    }, [ctx.isItemLoading, ctx.itemsResponse.items])
 
-    return renderer()
+    const search = useCallback((page: number) => {
+        searchItems({
+            name: ctx.searchWord,
+            page: page,
+        },
+            ctx.setItemLoading,
+            ctx.setItemsResponse
+        )
+    }, [searchItems, ctx.setItemLoading, ctx.setItemsResponse, ctx.currentPage, ctx.setSearchWord])
+
+    const goNext = useCallback(() => {
+        const nextPage = Math.min(ctx.currentPage + 1, ctx.itemsResponse.total)
+        ctx.setCurrentPage(nextPage)
+        search(nextPage)
+    }, [ctx.setCurrentPage, ctx.currentPage, ctx.itemsResponse.total])
+
+    const goPrev = useCallback(() => {
+        const prevPage = Math.max(0, ctx.currentPage - 1)
+        ctx.setCurrentPage(prevPage)
+        search(prevPage)
+    }, [ctx.setCurrentPage, ctx.currentPage])
+
+    const isNextDisabled = useMemo(() => {
+        return ctx.currentPage === ctx.itemsResponse.total || ctx.isItemLoading
+    }, [ctx.currentPage, ctx.itemsResponse.total, ctx.isItemLoading])
+
+    const isPrepDisabled = useMemo(() => {
+        return ctx.currentPage === 0 || ctx.isItemLoading
+    }, [ctx.currentPage, ctx.isItemLoading])
+
+
+    return <>
+        {itemsRenderer()}
+        <div>
+            {ctx.currentPage}
+            <button onClick={goPrev} disabled={isPrepDisabled}>
+                prev
+            </button>
+            <button onClick={goNext} disabled={isNextDisabled}>
+                next
+            </button>
+        </div>
+    </>
 }
--- src/pages/SearchPage/SearchForm.tsx
+++ src/pages/SearchPage/SearchForm.tsx
@@ -1,22 +1,24 @@
 import React, { useCallback, useContext, useState } from "react";
-import { SearchItemPageContext, SearchItemPageProps, SearchItemPage } from "./Page";
+import { SearchItemPageContext, SearchItemPageProps } from "./Page";
 import { searchItemsImpl } from "../../reqs/searchItems";
 
 type Props = Pick<SearchItemPageProps, "searchItems">
 
 export const SearchForm: React.FC<Props> = ({ searchItems = searchItemsImpl }) => {
-    const [searchWord, setSearchWord] = useState<string>("")
-
     const ctx = useContext(SearchItemPageContext)
 
+    const [searchWord, setSearchWord] = useState<string>(ctx.searchWord)
+
     const search = useCallback(() => {
+        ctx.setSearchWord(searchWord)
         searchItems({
             name: searchWord,
+            page: ctx.currentPage,
         },
             ctx.setItemLoading,
-            ctx.setItems
+            ctx.setItemsResponse
         )
-    }, [searchWord, searchItems, ctx.setItemLoading, ctx.setItems])
+    }, [searchWord, searchItems, ctx.setItemLoading, ctx.setItemsResponse, ctx.currentPage, ctx.setSearchWord])
 
     const setSearch = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchWord(e.target.value)
@@ -24,8 +26,8 @@ export const SearchForm: React.FC<Props> = ({ searchItems = searchItemsImpl }) =
 
     return <>
         <input value={searchWord} onChange={setSearch} />
-        <div onClick={search}>
+        <button onClick={search} disabled={ctx.isItemLoading}>
             search
-        </div>
+        </button>
     </>
 }
