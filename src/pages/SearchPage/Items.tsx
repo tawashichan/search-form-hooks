e0299af95b755f4425edd8b10b58a3fe0cca4021
irieryota <ryotairie123@gmail.com>
2019-09-30 01:33:25 +0900

    feat: itemない時にno itemsと表示

--- src/pages/SearchPage/Items.tsx
+++ src/pages/SearchPage/Items.tsx
@@ -6,17 +6,20 @@ export const Items: React.FC = () => {
     const ctx = useContext(SearchItemPageContext)
 
     const renderer = useCallback(() => {
-        if (ctx.isItemLoading) {
-            return <div>loading...</div>
-        } else {
-            const items = ctx.items.map((item, i, all) =>
-                <div key={item.name}>
-                    {item.name}
+        switch (true) {
+            case ctx.isItemLoading:
+                return <div>loading...</div>
+            case ctx.items.length === 0:
+                return <div>no items</div>
+            default:
+                const items = ctx.items.map((item, i, all) =>
+                    <div key={item.name}>
+                        {item.name}
+                    </div>
+                )
+                return <div>
+                    {items}
                 </div>
-            )
-            return <div>
-                {items}
-            </div>
         }
     }, [ctx.isItemLoading, ctx.items])
 
